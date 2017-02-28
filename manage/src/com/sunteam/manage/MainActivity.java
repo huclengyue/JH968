0a2a25310990341db930bedc970e663f3aa5a507
zbc <zhubaochao08@163.com>
2017-02-28 17:08:17 +0800

    增加在完全退出

--- manage/src/com/sunteam/manage/MainActivity.java
+++ manage/src/com/sunteam/manage/MainActivity.java
@@ -115,6 +115,8 @@ public class MainActivity extends MenuActivity implements ShowView{
 		if (TtsUtils.getInstance() != null) {
 			TtsUtils.getInstance().destroy();
 		}
+		android.os.Process.killProcess(android.os.Process.myPid());
+		System.exit(1);
 	}
 	@Override
 	protected void onResume() {
